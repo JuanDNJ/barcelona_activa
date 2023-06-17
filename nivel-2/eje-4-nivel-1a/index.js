5f8dc6381371ad33a1dc74070eef078e9bf5afb3
JuanDNJ <juandevnjv@gmail.com>
2023-06-17 08:04:26 +0200

    Refactorizando.

--- nivel-2/eje-4-nivel-1a/index.js
+++ nivel-2/eje-4-nivel-1a/index.js
@@ -1,8 +1,3 @@
-import './components/Menu.js'
-import './components/RedSocial.js'
-import './components/LogoEmpresa.js'
-import './components/BannerPrincipal.js'
-
 const $ = (tag, all = false) => {
   if (all === true) {
     return Array.from(document.querySelectorAll(tag))
